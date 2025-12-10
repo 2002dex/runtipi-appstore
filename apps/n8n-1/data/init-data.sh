c5349395fb7e96d913467e2fe65b72f4b03bd8ac
Deep Patel <deep.patel@dspworks.in>
2025-12-10 14:14:35 +0530

    n8n revert all changes to old version

--- apps/n8n-1/data/init-data.sh
+++ apps/n8n-1/data/init-data.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e;
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-EOSQL
-
 if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}" ]; then
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 		CREATE USER ${POSTGRES_NON_ROOT_USER} WITH PASSWORD '${POSTGRES_NON_ROOT_PASSWORD}';
